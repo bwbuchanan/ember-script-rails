41bdd3922726c7c792400de75f6d51431b2a740f
Brian Buchanan <brian@tiogalake.com>
2013-07-03 23:34:43 -0700

    Remove dependency on unset config var

--- lib/ember_script/rails/engine.rb
+++ lib/ember_script/rails/engine.rb
@@ -4,14 +4,12 @@ module EmberScript
       config.ember_script = ActiveSupport::OrderedOptions.new
 
       config.before_initialize do |app|
-        if app.config.assets.enabled
-          sprockets = if ::Rails::VERSION::MAJOR == 4
-            Sprockets.respond_to?('register_engine') ? Sprockets : app.assets
-          else
-            app.assets
-          end
-          sprockets.register_engine '.em', EmberScript::EmberScriptTemplate
+        sprockets = if ::Rails::VERSION::MAJOR == 4
+          Sprockets.respond_to?('register_engine') ? Sprockets : app.assets
+        else
+          app.assets
         end
+        sprockets.register_engine '.em', EmberScript::EmberScriptTemplate
       end
     end
   end
